b314741bcaf4aac154731c0786e2fb9532feea10
Roberto Filho <roberto.siilva@hotmail.com>
2023-02-16 10:20:10 -0300

    Initial Commit

---
+++ Conversores/ConversoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversoresApp.swift
+//  Conversores
+//
+//  Created by Roberto Filho on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ConversoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
